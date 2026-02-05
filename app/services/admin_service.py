5eb6c9955b0460c38001da3f6dab15618c4d094d
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 23:19:38 +0100

    definition de fonctions pour les services administrateurs

--- app/services/admin_service.py
+++ app/services/admin_service.py
@@ -1,3 +1,20 @@
 # creer un admin
 #bloquer user
 #voir les statistiques
+
+from functools import wraps
+from flask import redirect, url_for, flash
+from flask_login import current_user
+
+def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return redirect(url_for("auth.login"))
+
+        if not current_user.is_admin:
+            flash("Accès refusé")
+            return redirect(url_for("main.home"))
+
+        return func(*args, **kwargs)
+    return wrapper

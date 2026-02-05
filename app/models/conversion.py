66401972417aa791489318dd7669a9f86076fb7b
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 22:44:46 +0100

    ajout du temps actuel a la table conversion

--- app/models/conversion.py
+++ app/models/conversion.py
@@ -7,7 +7,7 @@ class conversion(db.Model):
     from_currency = db.Column(db.String(3), nullable=False)
     to_currency = db.Column(db.String(3), nullable=False)
     rate = db.Column(db.Float, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     type = db.Column(db.String(3), nullable=False)  #csv pour dataset et cov pour conversion
     def __init__(self, user_id, from_currency, to_currency, rate, date):
         self.user_id = user_id

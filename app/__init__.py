d509972c00b72316e15ae75564c8517bde66678f
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 23:11:10 +0100

    declararion de la fonction pour charger un utilsateur

--- app/__init__.py
+++ app/__init__.py
@@ -42,4 +42,8 @@ def create_app():
     app.register_blueprint(admin_bp)
     app.register_blueprint(main_bp)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     return app

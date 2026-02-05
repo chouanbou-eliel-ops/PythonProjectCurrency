93db2818fc7e0f51c566de1133f4590a711ba435
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 15:43:12 +0100

    ajout des migrations set des outils de gestion de connexion

--- app/extensions.py
+++ app/extensions.py
@@ -3,3 +3,10 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
+
+#migration
+migrate = Migrate()
+
+#gestion de connexion
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'

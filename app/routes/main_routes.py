28934fb95c7a629ce4fb9b5a025215cadd6e4bfb
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 18:17:54 +0100

    creation de la route vers la previsualisation des datasets

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -5,3 +5,21 @@ main_bp= Blueprint('main_bp', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route("/preview-csv", methods=["POST"])
+def preview_csv():
+    file = request.files.get("file")
+
+    if not file:
+        return render_template("preview.html", error="Aucun fichier")
+
+    df = pd.read_csv(file)
+
+    preview = df.head(10).to_dict(orient="records")
+    columns = df.columns.tolist()
+
+    return render_template(
+        "preview.html",
+        columns=columns,
+        preview=preview
+    )

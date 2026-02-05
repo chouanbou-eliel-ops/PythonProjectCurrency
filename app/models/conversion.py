3f16e65a8d4115d09f39d67ae929f1dc40e77195
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 23:03:58 +0100

    ajout de l'attribut type

--- app/models/conversion.py
+++ app/models/conversion.py
@@ -9,7 +9,8 @@ class Conversion(db.Model):
     to_currency = db.Column(db.String(3), nullable=False)
     rate = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    type = db.Column(db.String(3), nullable=False)  #csv pour dataset et cov pour conversion
+    type = db.Column(db.String(3), nullable=False)  #csv pour dataset et cov pour Conversion
+
     def __init__(self, user_id, from_currency, to_currency, rate, date):
         self.user_id = user_id
         self.from_currency = from_currency

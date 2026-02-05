40775c51345374092d72651f098c6255aa631403
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 21:41:35 +0100

    ajout du type de "conversion et commentaire associé"

--- app/models/conversion.py
+++ app/models/conversion.py
@@ -8,6 +8,7 @@ class conversion(db.Model):
     to_currency = db.Column(db.String(3), nullable=False)
     rate = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
+    type = db.Column(db.String(3), nullable=False)  #csv pour dataset et cov pour conversion
     def __init__(self, user_id, from_currency, to_currency, rate, date):
         self.user_id = user_id
         self.from_currency = from_currency

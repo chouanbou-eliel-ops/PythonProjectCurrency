4f41f259ce9b0073dbd7ec8bec7db19d0e3a0b74
Eliel Weme <chouanboueliel2008@gmail.com>
2026-02-05 21:46:15 +0100

    ajout des propriétés is_admin à la table User

--- app/models/user.py
+++ app/models/user.py
@@ -7,3 +7,6 @@ class User (UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(20), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    def __repr__(self):
+        return f"<User {self.username}>"
